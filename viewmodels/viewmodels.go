b411c9c797d20ada5d7c9835d42cfe545beb21e3
Robert Mang <Robert.Mang@softgarden.de>
2023-12-20 17:43:17 +0100

    move viewmodels to own package

---
+++ viewmodels/viewmodels.go
@@ -0,0 +1,12 @@
+package viewmodels
+
+type ChatMessageViewModel struct {
+	Username string
+	DateTime string
+	Message  string
+}
+
+type ChatRoomViewModel struct {
+	Username     string
+	ChatRoomName string
+}
