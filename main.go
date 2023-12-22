40219b7c0d5fa0235dd225872c724b549704ed62
Robert Mang <Robert.Mang@softgarden.de>
2023-12-22 20:01:52 +0100

    fix logs

--- main.go
+++ main.go
@@ -45,12 +45,12 @@ func getPort() string {
 
 	portNumber, err := strconv.Atoi(port)
 	if err != nil {
-		logger.Log.Info("Env variable GCHAT_PORT %s is not a valid integer. Use default port 8080.")
+		logger.Log.Infof("Env variable GCHAT_PORT %s is not a valid integer. Use default port 8080.", port)
 		return "8080"
 	}
 
 	if portNumber > 65536 {
-		logger.Log.Info("Provided port number %d is invalid, because it is larger than 65536. Use default port 8080.")
+		logger.Log.Infof("Provided port number %d is invalid, because it is larger than 65536. Use default port 8080.", portNumber)
 		return "8080"
 	}
 
